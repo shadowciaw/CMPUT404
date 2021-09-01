c000a53992b1621f2f915fb3c799caac7cc683fa
shadowciaw <shadowcharmeleon@gmail.com>
2021-09-01 16:39:59 -0600

    Updated lab1 python script

--- lab1/python_script.py
+++ lab1/python_script.py
@@ -2,4 +2,7 @@ import requests
 
 print(requests.__version__)
 response = requests.get('http://google.com')
+
+# download itself from github and print source code
+response = requests.get('https://raw.githubusercontent.com/shadowciaw/CMPUT404/main/lab1/python_script.py?token=AKJ443XF5PPXZ76NKMNON4LBHE5Z4')
 print(response.text)
