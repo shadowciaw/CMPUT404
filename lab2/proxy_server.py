e45783372daac77c9cd9eb7cf085a9917b48127c
shadowciaw <shadowcharmeleon@gmail.com>
2021-09-09 22:51:19 -0600

    factored out google connection

--- lab2/proxy_server.py
+++ lab2/proxy_server.py
@@ -39,40 +39,40 @@ def get_remote_ip(host):
 
     print(f'Ip address of {host} is {remote_ip}')
     return remote_ip
+    print("Payload sent successfully")
 
 
-def send_data_google(serversocket, payload):
-    """
-        send data to www.google.com; payload in byte string
-    """
+def forward_to_google(request: bytes) -> bytes:
+    # make the socket, get the ip, and connect to google
+    google = create_tcp_socket()
+    google_ip = get_remote_ip(GOOGLE_HOST)
+    google.connect((google_ip, GOOGLE_PORT))
+    print(f'Socket Connected to {GOOGLE_HOST} on ip {google_ip}')
+
     print("Sending payload")
     try:
-        serversocket.sendall(payload)
+        google.sendall(request)
     except socket.error:
         print('Send failed')
         sys.exit()
     print("Payload sent successfully")
 
+    response = google.recv(BUFFER_SIZE)
+    google.close()
 
-def forward(serversocket: socket, clientsocket: socket):
+    return response
+
+def forward(clientsocket: socket):
     # accept data from connected client
-    full_data = clientsocket.recv(BUFFER_SIZE)
-    print(f"received data: {full_data}")
+    request = clientsocket.recv(BUFFER_SIZE)
+    print(f"received data: {request}")
+    
+    response = forward_to_google(request)
 
-    send_data_google(serversocket, full_data)
-    full_data = b""
-    while True:
-        data = serversocket.recv(BUFFER_SIZE)
-        if not data:
-            break
-        full_data += data
-        print(f"recv'd {data}")
-
-    clientsocket.sendall(full_data)
+    clientsocket.sendall(response)
     print("sent back data")
     clientsocket.close()
     sys.exit(0)  # kill the child process
-
 
 def main():
     """
@@ -82,12 +82,6 @@ def main():
     4. send response from www.google.com to client
     """
     try:
-        # make the socket, get the ip, and connect to google
-        google = create_tcp_socket()
-        google_ip = get_remote_ip(GOOGLE_HOST)
-        google.connect((google_ip, GOOGLE_PORT))
-        print(f'Socket Connected to {GOOGLE_HOST} on ip {google_ip}')
-
         # make the listening socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -99,7 +93,7 @@ def main():
             conn, addr = s.accept()
             print(f"Connected by {addr}")
 
-            p = Process(target=forward, args=(google, conn))
+            p = Process(target=forward, args=(conn))
             p.start()
     except Exception as e:
         print("EXCEPTION: ", e)
